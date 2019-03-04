6273a0f5202ca096b1bda0bae1b75ebf9d849fc6
wayan29 <44164208+wayan29@users.noreply.github.com>
2019-03-04 17:17:56 +0800

    Add files via upload

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+
+/* recomended 40 sec */
+$set_delay=40;
+
+$config=[
+"imei" => "868835032429110",
+"sign" => "57a3e669db5f20e477d69387c16ac983",
+"uuid" => "3059689615c642d6",
+"token" => "c55622d91eca00719292dba821042c10"
+];
